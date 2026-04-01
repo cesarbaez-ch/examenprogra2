2f8b23080666c2d470dce1bc745210311cba7817
Cesar Baez <cesargadielbaez@gmail.com>
2026-04-01 18:15:16 -0400

    "Proyecto Finalizado - Clase Libro y Program"

--- examenprogra2/Libro.cs
+++ examenprogra2/Libro.cs
@@ -1,10 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace examenprogra2
+public class Libro
 {
-    internal class Libro
+    private readonly string _codigo;
+    private string _titulo;
+    private string _autor;
+    private int _anioPublicacion;
+    private bool _estaPrestado;
+    private int _contadorPrestamos;
+
+    public string Codigo => _codigo;
+    public string Titulo { get => _titulo; set => _titulo = value; }
+    public string Autor { get => _autor; set => _autor = value; }
+    public int AnioPublicacion { get => _anioPublicacion; set => _anioPublicacion = value; }
+    public string Estado => _estaPrestado ? "Prestado" : "Disponible";
+
+    public Libro(string codigo, string titulo, string autor, int anio)
     {
+        _codigo = codigo;
+        _titulo = titulo;
+        _autor = autor;
+        _anioPublicacion = anio;
+        _estaPrestado = false;
+        _contadorPrestamos = 0;
+    }
+
+    public void Prestar()
+    {
+        if (!_estaPrestado)
+        {
+            _estaPrestado = true;
+            _contadorPrestamos++;
+            Console.WriteLine($"Libro {_titulo} prestado.");
+        }
+        else
+        {
+            Console.WriteLine("No disponible.");
+        }
+    }
+
+    public void Devolver()
+    {
+        _estaPrestado = false;
+        Console.WriteLine($"Libro {_titulo} devuelto.");
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"{_codigo} | {_titulo} | {_autor} | {Estado}");
+    }
+
+    public void ObtenerEstadisticas()
+    {
+        Console.WriteLine($"Préstamos de '{_titulo}': {_contadorPrestamos}");
+    }
+
+    ~Libro()
+    {
+        Console.WriteLine($"Objeto {_codigo} eliminado.");
     }
 }
--- examenprogra2/Program.cs
+++ examenprogra2/Program.cs
@@ -1,66 +1,5 @@
 ﻿using System;
 
-public class Libro
-{
-    private readonly string _codigo;
-    private string _titulo;
-    private string _autor;
-    private int _anioPublicacion;
-    private bool _estaPrestado;
-    private int _contadorPrestamos;
-
-    public string Codigo => _codigo;
-    public string Titulo { get => _titulo; set => _titulo = value; }
-    public string Autor { get => _autor; set => _autor = value; }
-    public int AnioPublicacion { get => _anioPublicacion; set => _anioPublicacion = value; }
-    public string Estado => _estaPrestado ? "Prestado" : "Disponible";
-
-    public Libro(string codigo, string titulo, string autor, int anio)
-    {
-        _codigo = codigo;
-        _titulo = titulo;
-        _autor = autor;
-        _anioPublicacion = anio;
-        _estaPrestado = false;
-        _contadorPrestamos = 0;
-    }
-
-    public void Prestar()
-    {
-        if (!_estaPrestado)
-        {
-            _estaPrestado = true;
-            _contadorPrestamos++;
-            Console.WriteLine($"Libro {_titulo} prestado.");
-        }
-        else
-        {
-            Console.WriteLine("No disponible.");
-        }
-    }
-
-    public void Devolver()
-    {
-        _estaPrestado = false;
-        Console.WriteLine($"Libro {_titulo} devuelto.");
-    }
-
-    public void MostrarInformacion()
-    {
-        Console.WriteLine($"{_codigo} | {_titulo} | {_autor} | {Estado}");
-    }
-
-    public void ObtenerEstadisticas()
-    {
-        Console.WriteLine($"Préstamos de '{_titulo}': {_contadorPrestamos}");
-    }
-
-    ~Libro()
-    {
-        Console.WriteLine($"Objeto {_codigo} eliminado.");
-    }
-}
-
 class Program
 {
     static void Main()
@@ -73,12 +12,14 @@ class Program
         l2.Prestar();
         l2.Devolver();
 
+        Console.WriteLine("\n--- ESTADO ACTUAL ---");
         l1.MostrarInformacion();
         l2.MostrarInformacion();
         l3.MostrarInformacion();
 
         l1.ObtenerEstadisticas();
 
+        Console.WriteLine("\nPresiona una tecla para salir...");
         Console.ReadKey();
     }
 }
